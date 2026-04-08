380b57a473b51aa499110cafe0c6ec23b2e58c33
Gabriel Menendez <menendez.gabriel@gmail.com>
2026-04-08 11:13:32 -0300

    Add icon for desktop app

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :photos, only: [ :index, :show, :new, :create, :destroy ]
 
   get "up" => "rails/health#show", as: :rails_health_check
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   root "photos#index"
 end
